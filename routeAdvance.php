7f3989cf5285c8e03983374a9961fae1ea18b0c7
Mateo Albert <mateo.albert45@gmail.com>
2018-09-19 00:56:04 -0300

    nuevo route

---
+++ routeAdvance.php
@@ -0,0 +1,36 @@
+<?php
+
+define('ACTION', 0);
+define('PARAMS', 1);
+
+require_once "config/ConfigApp.php";
+require_once "controller\TiendaController.php";
+
+function parseURL($url)
+{
+  $urlExploded = explode('/', $url);
+  $arrayReturn[ConfigApp::$ACTION] = $urlExploded[ACTION];
+  $arrayReturn[ConfigApp::$PARAMS] = isset($urlExploded[PARAMS]) ? array_slice($urlExploded,1) : null;
+  return $arrayReturn;
+}
+
+if(isset($_GET['action'])){
+    $urlData = parseURL($_GET['action']);
+    $action = $urlData[ConfigApp::$ACTION]; //home
+    if(array_key_exists($action,ConfigApp::$ACTIONS)){
+        $params = $urlData[ConfigApp::$PARAMS];
+        $action = explode('#',ConfigApp::$ACTIONS[$action]); //Array[0] -> TareasController [1] -> BorrarTarea
+        $controller =  new $action[0]();
+        $metodo = $action[1];
+        if(isset($params) &&  $params != null){
+            echo $controller->$metodo($params);
+        }
+        else{
+            echo $controller->$metodo();
+        }
+    }else{
+      $controller =  new TareasController();
+      echo $controller->Home();
+    }
+}
+ ?>
